cdd7f39421bf40176a9fa2e567eda63f03eeb3c9
Lassi Kortela <lassi@lassi.io>
2020-09-21 13:30:07 +0300

    Fix write-string compat

--- r6rs.scm
+++ r6rs.scm
@@ -1,17 +1,15 @@
 #!r6rs
 (import (rnrs) (only (chezscheme) pretty-print))
 
-(define (append-map mapfun xs)
-  (fold-left (lambda (acc x) (append acc (mapfun x))) '() xs))
+(define (append-map f xs)
+  (fold-left (lambda (acc x) (append acc (f x))) '() xs))
 
 (define (translate x)
   (case (car x)
-    ((import)
-     `((import (rename (rnrs) put-string write-string))))
-    ((begin)
-     (cdr x))
-    (else
-     (list x))))
+    ((import) `((import (rnrs))))
+    ((begin) `((define (write-string s) (put-string (current-output-port) s))
+               ,@(cdr x)))
+    (else `(,x))))
 
 (let ((r7rs-form (with-input-from-file "srfi-minimal-html.sld" read)))
   (let ((r6rs-form `(library ,@(append-map translate (cdr r7rs-form)))))
